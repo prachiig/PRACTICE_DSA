ca31b56093ed8ff1d488d30313b7ca5726c1d929
prachiig <prachi-ai-ig@outlook.com>
2024-10-16 14:42:58 +0530

    Add files via upload

--- practice1.java
+++ practice1.java
@@ -10,14 +10,27 @@ class Node{
 
 
 public class practice1 {
-    public static void traverseLL(Node head){
 
+
+    // TC - O(N)
+    // SC- 0(1)
+    public static void traverseLL(Node head){
         Node ptr = head;
         while(ptr != null){
             System.out.print(ptr.data+" ");
             ptr = ptr.next;
         }
-        
+    }
+
+    public static void traverseLL_recurcise(Node head){
+        Node ptr = head;
+        if(ptr == null){
+            return;
+        }
+        else{
+            System.out.print(ptr.data+" ");
+            traverseLL_recurcise(ptr.next);
+        }
     }
 
     public static void main(String[] args) {
@@ -25,6 +38,9 @@ public class practice1 {
         head.next = new Node(20);
         head.next.next = new Node(30);
         head.next.next.next = new Node(40);
+        System.out.println("Non-recursive approach:");
         traverseLL(head);
+        System.out.println("\n"+"Recursive approach:");
+        traverseLL_recurcise(head);
     }
 }
